930a8378902346200656cdf2e506bdca63c52745
Kristijan <kristijan.bartol@gmail.com>
2017-11-17 18:55:05 +0100

    Added third party 7digital preview music downloader

---
+++ third_party/__init__.py
@@ -0,0 +1,6 @@
+# code in this directory modified based on https://github.com/mlachmish/MusicGenreClassification
+
+__author__ = "Matan Lachmish"
+__copyright__ = "Copyright 2016, Tel Aviv University"
+__version__ = "1.0"
+__status__ = "Development"
