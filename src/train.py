50e6d42545b23df5a548f3819889e5273b6f9482
Kristijan <kristijan.bartol@gmail.com>
2017-12-18 10:34:11 +0100

    Using original model builder again. Fixed bug with odd input dimensions.

--- src/train.py
+++ src/train.py
@@ -1,70 +1,87 @@
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, ELU, GRU, Activation
+from keras import backend as K
+from keras.models import Model
+from keras.layers import Dense, Dropout, ELU, GRU
 from keras.layers import ZeroPadding2D, Conv2D, MaxPooling2D
 from keras.layers import BatchNormalization, Reshape
 from keras.optimizers import SGD
 
+import numpy as np
+
 import input_data
+
+batch_size = 50
+img_height = 96
+img_width = 1366
+channels = 1
 
 channel_axis = 3
 freq_axis = 1
 
+padding = 37
+
+
+def build_model(input_tensor):
+    # Input shape: (batch, height, width, channels)
+    # Input block
+    x = ZeroPadding2D(padding=(0, padding), data_format='channels_last')(input_tensor)
+
+    x = BatchNormalization(axis=freq_axis, name='bn_0_freq')(x)
+
+    # Conv block 1
+    x = Conv2D(64, (3, 3), padding='same', name='conv1')(x)
+    x = BatchNormalization(axis=channel_axis, name='bn1')(x)
+    x = ELU()(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool1')(x)
+    x = Dropout(0.1, name='dropout1')(x)
+
+    # Conv block 2
+    x = Conv2D(128, (3, 3), padding='same', name='conv2')(x)
+    x = BatchNormalization(axis=channel_axis, name='bn2')(x)
+    x = ELU()(x)
+    x = MaxPooling2D(pool_size=(3, 3), strides=(3, 3), name='pool2')(x)
+    x = Dropout(0.1, name='dropout2')(x)
+
+    # Conv block 3
+    x = Conv2D(128, (3, 3), padding='same', name='conv3')(x)
+    x = BatchNormalization(axis=channel_axis, name='bn3')(x)
+    x = ELU()(x)
+    x = MaxPooling2D(pool_size=(4, 4), strides=(4, 4), name='pool3')(x)
+    x = Dropout(0.1, name='dropout3')(x)
+
+    # Conv block 4
+    x = Conv2D(128, (3, 3), padding='same', name='conv4')(x)
+    x = BatchNormalization(axis=channel_axis, name='bn4')(x)
+    x = ELU()(x)
+    x = MaxPooling2D(pool_size=(4, 4), strides=(4, 4), name='pool4')(x)
+    x = Dropout(0.1, name='dropout4')(x)
+
+    x = Reshape((-1, 15, 128))(x)
+
+    # GRU block 1, 2, output
+    x = GRU(32, return_sequences=True, name='gru1')(x)
+    x = GRU(32, return_sequences=False, name='gru2')(x)
+    x = Dropout(0.3)(x)
+
+    x = Dense(50, activation='softmax', name='output')(x)
+
+    return Model(melgram_input, x)
+
+
+def multi_output_cross_entropy(labels, outputs):
+    loss_sum = 0
+    for idx, output in enumerate(outputs):
+        loss_sum += - labels[idx] * np.log(output)
+        return loss_sum / outputs.shape[0]
+
+
 data = input_data.get_data()
-x_train, y_train = data.train.all_loaded()
-x_test, y_test = data.test.all_loaded()
+# x_train, y_train = data.train.all_loaded()
+# x_test, y_test = data.test.all_loaded()
 
-model = Sequential()
-
-# input: 96x1366 images with grayscale channel
-model.add(ZeroPadding2D(padding=(0, 37), input_shape=(96, 1366, 1)))
-model.add(BatchNormalization(axis=freq_axis, name='bn_0_freq'))
-
-# Conv block 1
-model.add(Conv2D(64, 3, 3, border_mode='same', name='conv1'))
-model.add(BatchNormalization(axis=channel_axis, mode=0, name='bn1'))
-model.add(ELU())
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool1'))
-model.add(Dropout(0.1, name='dropout1'))
-
-# Conv block 2
-model.add(Conv2D(128, 3, 3, border_mode='same', name='conv2'))
-model.add(BatchNormalization(axis=channel_axis, mode=0, name='bn2'))
-model.add(ELU())
-model.add(MaxPooling2D(pool_size=(3, 3), strides=(3, 3), name='pool2'))
-model.add(Dropout(0.1, name='dropout2'))
-
-# Conv block 3
-model.add(Conv2D(128, 3, 3, border_mode='same', name='conv3'))
-model.add(BatchNormalization(axis=channel_axis, mode=0, name='bn3'))
-model.add(ELU())
-model.add(MaxPooling2D(pool_size=(4, 4), strides=(4, 4), name='pool3'))
-model.add(Dropout(0.1, name='dropout3'))
-
-# Conv block 4
-model.add(Conv2D(128, 3, 3, border_mode='same', name='conv4'))
-model.add(BatchNormalization(axis=channel_axis, mode=0, name='bn4'))
-model.add(ELU())
-model.add(MaxPooling2D(pool_size=(4, 4), strides=(4, 4), name='pool4'))
-model.add(Dropout(0.1, name='dropout4'))
-
-# prepare dimensions for GRUs
-model.add(Reshape((15, 128)))
-
-# GRU block 1
-model.add(GRU(32, return_sequences=True, name='gru1'))
-
-# GRU block 2
-model.add(GRU(32, return_sequences=False, name='gru2'))
-model.add(Dropout(0.3))
-
-# Dense layer
-model.add(Dense(50, name='logits'))
-
-# apply softmax to get probabilistic outputs
-model.add(Activation('softmax', name='softmax'))
+model = build_model(K.placeholder(shape=(batch_size, img_height, img_width, channels)))
 
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd)
+model.compile(loss=multi_output_cross_entropy, optimizer=sgd)
 
-model.fit(x_train, y_train, batch_size=32, epochs=10)
-score = model.evaluate(x_test, y_test, batch_size=32)
+model.fit(data.train.next_batch(batch_size), data.train.labels, batch_size=batch_size, epochs=1)
+score = model.evaluate(data.test.next_batch(batch_size), data.test.labels, batch_size=batch_size)
