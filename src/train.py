a6852dcd1dbc3ac8e8134a021b835a65086620a2
Kristijan <kristijan.bartol@gmail.com>
2017-12-18 12:50:04 +0100

    Fixed incompatible dimensions on GRU call. However, first bug was not correctly fixed.

--- src/train.py
+++ src/train.py
@@ -1,11 +1,12 @@
 from keras import backend as K
 from keras.models import Model
-from keras.layers import Dense, Dropout, ELU, GRU
+from keras.layers import Input, Dense, Dropout, ELU, GRU
 from keras.layers import ZeroPadding2D, Conv2D, MaxPooling2D
 from keras.layers import BatchNormalization, Reshape
 from keras.optimizers import SGD
 
 import numpy as np
+import tensorflow as tf
 
 import input_data
 
@@ -21,10 +22,15 @@ padding = 37
 
 
 def build_model(input_tensor):
-    # Input shape: (batch, height, width, channels)
+    # Convert tf tensor to keras
+    if not K.is_keras_tensor(input_tensor):
+        input_tensor = Input(tensor=input_tensor, shape=(batch_size, img_height, img_width, channels))
+
+    # TODO: (temporal fix)  After Input function call, I ran into a bug with 'None' dimension
+    input_tensor._keras_shape = [dim for dim in input_tensor._keras_shape if dim is not None]
+
     # Input block
     x = ZeroPadding2D(padding=(0, padding), data_format='channels_last')(input_tensor)
-
     x = BatchNormalization(axis=freq_axis, name='bn_0_freq')(x)
 
     # Conv block 1
@@ -55,7 +61,8 @@ def build_model(input_tensor):
     x = MaxPooling2D(pool_size=(4, 4), strides=(4, 4), name='pool4')(x)
     x = Dropout(0.1, name='dropout4')(x)
 
-    x = Reshape((-1, 15, 128))(x)
+    # (50, 1, 15, 128) -> (50, 15, 128)
+    x = tf.reshape(x, [batch_size, 15, 128])
 
     # GRU block 1, 2, output
     x = GRU(32, return_sequences=True, name='gru1')(x)
